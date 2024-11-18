341dca69324605ea1906e90f2de363e270fe84a8
dermotte <mathias.lux@gmail.com>
2024-11-18 16:24:09 +0100

    initial commit

---
+++ request.js
@@ -0,0 +1,27 @@
+const axios = require("axios")
+
+async function scrapeWebsite(url) {
+    try {
+        const response = await axios.get(url);
+        const xml = response.data;
+
+        var parseString = require('xml2js').parseString;
+        parseString(xml, function (err, result) {
+            // console.dir(result);
+
+            for (let article of result["rdf:RDF"].item) {
+                if (!(typeof article.description === "undefined"))
+                     console.log(article.title[0] + ": " + article.description)
+                else
+                    console.log(article.title[0] + ": " + article.link)
+            }
+        
+            // console.log("Done.")
+        });
+    } catch (error) {
+        console.error("Error fetching website data:", error);
+    }
+}
+
+// Example usage:
+scrapeWebsite("https://rss.orf.at/news.xml");
